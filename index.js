9f571c4fda7b6415789d96dd80433da30a88d8dc
Rachel <rachtuch00@gmail.com>
2022-12-01 14:08:22 -0500

    commit tree fc3faf80e7d5e781baec03c6a257e74cc14a3ceb

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction("Hi my name is Aki.") {
+    console.log(function introduction);
+}
+
+
+function introductionWithLanguage(name, language) {
+    console.log(function introductionWithLanguage("Aki, "Ember.js"));
+}
+ 
+function introductionWithLanguageOptional(name,language) {
+    console.log(function introductionWithLanguageOptional("Gracie"));
+}
+
+function introductionWithLanguageOptional(name, language) {
+    console.log(function introductionWithLanguageOptional("Gracie", "Python"));
+}
+
+
